85cc74c2c91341868e39e2745e1e7637a464eba2
Stefanus Adri Irawan <148773999+stefanus-ai-tech@users.noreply.github.com>
2025-01-03 15:01:07 +0700

    removed about

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import Index from "./pages/Index";
-import Instruments from "./pages/Instruments";
-import FilmScoring from "./pages/FilmScoring";
-import Commercial from "./pages/Commercial";
-import SFX from "./pages/SFX";
-import Production from "./pages/Production";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import Index from './pages/Index';
+import Instruments from './pages/Instruments';
+import FilmScoring from './pages/FilmScoring';
+import Commercial from './pages/Commercial';
+import SFX from './pages/SFX';
+import Production from './pages/Production';
+import Contact from './pages/Contact';
 
 const queryClient = new QueryClient();
 
@@ -29,7 +28,6 @@ const App = () => (
             <Route path="/commercial" element={<Commercial />} />
             <Route path="/sfx" element={<SFX />} />
             <Route path="/production" element={<Production />} />
-            <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </Layout>
